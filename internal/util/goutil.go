fe21d35be9775d1c1f8d509a1d0402ca82431c04
gaggad <gaggad@163.com>
2024-11-07 17:19:46 +0800

    init

---
+++ internal/util/goutil.go
@@ -0,0 +1,81 @@
+package util
+
+import (
+	"bytes"
+	"fmt"
+	"html/template"
+	"os"
+	"path/filepath"
+	"runtime"
+)
+
+// PanicToError Panic转换为error
+func PanicToError(f func()) (err error) {
+	defer func() {
+		if e := recover(); e != nil {
+			err = fmt.Errorf(PanicTrace(e))
+		}
+	}()
+	f()
+	return
+}
+
+// PanicTrace panic调用链跟踪
+func PanicTrace(err interface{}) string {
+	stackBuf := make([]byte, 4096)
+	n := runtime.Stack(stackBuf, false)
+
+	return fmt.Sprintf("panic: %v %s", err, stackBuf[:n])
+}
+
+// PrintAppVersion 打印应用版本
+func PrintAppVersion(appVersion, GitCommit, BuildDate string) {
+	versionInfo, err := FormatAppVersion(appVersion, GitCommit, BuildDate)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(versionInfo)
+}
+
+// FormatAppVersion 格式化应用版本信息
+func FormatAppVersion(appVersion, GitCommit, BuildDate string) (string, error) {
+	content := `
+   Version: {{.Version}}
+Go Version: {{.GoVersion}}
+Git Commit: {{.GitCommit}}
+     Built: {{.BuildDate}}
+   OS/ARCH: {{.GOOS}}/{{.GOARCH}}
+`
+	tpl, err := template.New("version").Parse(content)
+	if err != nil {
+		return "", err
+	}
+	var buf bytes.Buffer
+	err = tpl.Execute(&buf, map[string]string{
+		"Version":   appVersion,
+		"GoVersion": runtime.Version(),
+		"GitCommit": GitCommit,
+		"BuildDate": BuildDate,
+		"GOOS":      runtime.GOOS,
+		"GOARCH":    runtime.GOARCH,
+	})
+	if err != nil {
+		return "", err
+	}
+
+	return buf.String(), err
+}
+
+// WorkDir 获取程序运行时根目录
+func WorkDir() (string, error) {
+	execPath, err := os.Executable()
+	if err != nil {
+		return "", err
+	}
+	wd := filepath.Dir(execPath)
+	if filepath.Base(wd) == "bin" {
+		wd = filepath.Dir(wd)
+	}
+
+	return wd, nil
+}
